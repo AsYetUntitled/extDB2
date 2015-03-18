a9400563088a53283c7304170ea3a4100b7d6496
Torndeco <torndeco.di@gmail.com>
2015-03-18 05:12:05 +0000

    Fix: re-add #include boost thread

--- src/ext.h
+++ src/ext.h
@@ -22,6 +22,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #include <unordered_map>
 
 #include <boost/asio.hpp>
+#include <boost/thread/thread.hpp>
 
 #include <Poco/Data/SessionPool.h>
 
