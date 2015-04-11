b9a30e10f6cf06c48ffc67b646c87c01be040f18
Torndeco <torndeco.di@gmail.com>
2015-04-11 02:16:56 +0100

    Bumped extDB2 Version Number

--- src/abstract_ext.h
+++ src/abstract_ext.h
@@ -31,7 +31,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #include "spdlog/spdlog.h"
 
 
-#define EXTDB_VERSION "49"
+#define EXTDB_VERSION "50"
 #define EXTDB_CONF_VERSION 3
 
 class RedisWorker;
